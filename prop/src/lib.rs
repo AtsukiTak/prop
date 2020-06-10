f7ccad891637be063e147472076957b028280492
AtsukiTak <takatomgoo@gmail.com>
2020-06-10 16:52:18 +0900

    add document

--- prop/src/lib.rs
+++ prop/src/lib.rs
@@ -8,6 +8,11 @@ pub mod ident;
 /// - `R<E<Q<__<B<O<D<Y<()>>>>>>>>` represents `req_body` property.
 pub trait PropIdent {}
 
+/// This trait represents that implemented struct has a property whose identification is
+/// represented by `Ident` and type is `Type`.
+///
+/// Suppose we have a struct `Foo` which has a property `foo: usize`, then `Foo` can implement
+/// `Property<F<O<O<()>>>, usize>`.
 pub trait Property<Ident, Type>: PropertyBase
 where
     Ident: PropIdent,
@@ -29,10 +34,37 @@ where
     fn take_only_once(&mut self) -> Type;
 }
 
+/// This trait is much like `From` of standard library.
+/// The difference is that this trait represents property based conversion.
+///
+/// This will automatically be implemented like below but generated code is slightly diferrent.
+///
+/// ```ignore
+/// struct Foo {
+///     foo: usize,
+///     bar: String,
+/// }
+///
+/// type FOO = F<O<O<()>>>;
+/// type BAR = B<A<R<()>>>;
+///
+/// impl<T> PropFrom<T> for Foo
+/// where
+///     T: Property<FOO, usize> + Property<BAR, usize>
+/// {
+///     fn prop_from(t: T) -> Self {
+///         todo!()
+///     }
+/// }
+/// ```
 pub trait PropFrom<T> {
     fn prop_from(t: T) -> Self;
 }
 
+/// This trait is much like `Into` of standard library.
+/// The difference is that this trait represents property based conversion.
+///
+/// This is automatically implemented in most case.
 pub trait PropInto<T> {
     fn prop_into(self) -> T;
 }
